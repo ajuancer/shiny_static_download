54f59da9281c31a45804f6eac0d68a8ced567715
Juan <39202665+ajuancer@users.noreply.github.com>
2020-12-24 15:59:20 +0100

    changed name

--- ui.R
+++ ui.R
@@ -8,10 +8,10 @@ fluidPage(
 
   sidebarPanel(
     p('A Shiny application test to try out different ways of exporting data, to be able to serve it on a non-session-dependent route.'),
-    div(p(HTML(paste0('All the code and more information can be found at ', a(href = 'https://github.com/ajuancer/shinyapp_static_download', 'https://www.github.com/ajuancer/shinyapp_static_download'), '. Feel free to open an issue if you have any suggestion or problem.')))),
+    div(p(HTML(paste0('All the code and more information can be found at ', a(href = 'https://github.com/ajuancer/shiny_static_download', 'https://www.github.com/ajuancer/shiny_static_download'), '. Feel free to open an issue if you have any suggestion or problem.')))),
 
     h3('License'),
-    div(p(HTML(paste0('Under the MIT license. Please reffer to ', a(href = 'https://github.com/ajuancer/shinyapp_static_download/LICENSE.md', 'the repository lincense'), ' for more information.')))),
+    div(p(HTML(paste0('Under the MIT license. Please reffer to ', a(href = 'https://github.com/ajuancer/shiny_static_download/LICENSE.md', 'the repository lincense'), ' for more information.')))),
 
     div(p(HTML(paste0('The used database belongs to the REMEDHUS network, part of the ', a(href = 'https://ismn.geo.tuwien.ac.at/en/', 'ISMN'), '. This infrastructure is maintained by the ', a(href = 'http://campus.usal.es/~hidrus/index.htm', 'Water Resources Research Group of the University of Salamanca'),'.')))),
 
