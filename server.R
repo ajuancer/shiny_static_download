93c07dc14d00e6c45a8a4c85ae02a9f7e35bb6ea
Juan <39202665+ajuancer@users.noreply.github.com>
2020-12-24 14:01:02 +0100

    add static path
    add static path and load database before

--- server.R
+++ server.R
@@ -2,10 +2,14 @@ library(shiny)
 library(ggplot2)
 
 function(input, output) {
+  database <- load(file="./data/Granja.RData")
+
+  addResourcePath('export', 'export')
+  write.csv(granja, paste("./export/test_02.csv"), row.names = FALSE)
+
 
   output$plot <- renderPlot({
 
-    database <- load(file="data/Granja.RData")
     p <- plot(granja$TIMESTAMP,granja$VV_media)
 
     print(p)
